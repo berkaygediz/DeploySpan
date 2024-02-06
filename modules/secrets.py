dbd5140a6236c1f971bac5e3e09345a6b8679a3f
Berkay Gediz <121058050+berkaygediz@users.noreply.github.com>
2024-02-06 14:47:08 +0300

    ♻️ formatting, linting

--- modules/secrets.py
+++ modules/secrets.py
@@ -1,16 +1,17 @@
 import mysql.connector
 from mysql.connector import Error
 
-sql_host = 'localhost'
-sql_database = 'bgecosystem2'
-sql_user = 'root'
-sql_password = ''
+sql_host = "localhost"
+sql_database = "bgecosystem2"
+sql_user = "root"
+sql_password = ""
 
 
 def serverconnect():
     try:
         conn = mysql.connector.connect(
-            host=sql_host, database=sql_database, user=sql_user, password=sql_password)
+            host=sql_host, database=sql_database, user=sql_user, password=sql_password
+        )
         if conn.is_connected():
             return conn
         else:
